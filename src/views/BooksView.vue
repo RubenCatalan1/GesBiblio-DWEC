e202720614a3ad88e794701bfcc5ab6390681f36
Rubén Catalán <rucaju5@gmail.com>
2023-01-24 11:46:46 +0100

    GesBiblio-END

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -7,7 +7,21 @@
         data() {
             return {
                 books: [],
-                title: ""
+                title: "",
+                tema_id: "",
+            }
+        },
+        watch: {
+            async tema_id(newValue, oldValue) {
+                await this.mountData()
+                if(newValue === 'key') {
+                    return
+                }
+                this.filterBoooksByTemaId(newValue)
+                if(this.title === "Libros sin filtrar") {
+                    this.title = "Libros";
+                }
+                this.title = this.title + " del tema " + this.temas.find(elem => elem.id === parseInt(newValue)).nombre
             }
         },
 
@@ -22,19 +36,28 @@
            })
         },
         methods: {
-            ...mapActions(useDataStore, ['getAllBooks', 'getBooksByAuthorId'])
-        },
-        async mounted() {
-            if(this.$route.params.id) {
-                this.books = await this.getBooksByAuthorId(this.$route.params.id)
-                this.title = "Libros del autor " + this.getAuthorById(this.$route.params.id)
-            } else {
-                this.books = await this.getAllBooks()
-                this.title = "Libros sin filtrar"
+            ...mapActions(useDataStore, ['getAllBooks', 'getBooksByAuthorId']),
+
+            filterBoooksByTemaId($id) {
+                this.books = this.books.filter(elem => elem.tema === parseInt($id))
+            },
+
+            async mountData() {
+                if(this.$route.params.id) {
+                    this.books = await this.getBooksByAuthorId(this.$route.params.id)
+                    let autor = this.getAuthorById(this.$route.params.id)
+                    this.title = "Libros del autor " + autor.nombre + autor.apellidos
+                } else {
+                    this.books = await this.getAllBooks()
+                    this.title = "Libros sin filtrar"
+                }
             }
 
-        }
-
+        },
+        async mounted() {
+            this.mountData()
+        },
+        
     }
 </script>
 
@@ -44,6 +67,13 @@
             <h1 class="text-center">{{ title }}</h1>
         </div>
     </div>
+    <div class="row">
+        <select type="text" class="form-control" v-model="tema_id">
+            <option value="key">--Seleccione tema--</option>
+            <option v-for="elem in temas" :key="elem.id" :value="elem.id">{{ elem.nombre }}</option>
+        </select>
+    </div>
+  
     <div class="row m-2">
         <app-book v-for="elem in books" :key="elem.id" v-bind:book="elem"></app-book>
     </div>
